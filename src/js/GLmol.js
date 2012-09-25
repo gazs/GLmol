029525fd7edb7bf58266f42a172eab2fe79af3f6
Körtesi Gáspár <gazs@bergengocia.net>
2012-09-25 17:00:20 +0200

    assume that workaround is still needed

--- src/js/GLmol.js
+++ src/js/GLmol.js
@@ -34,9 +34,9 @@
         return !!(window.WebGLRenderingContext && (canvas.getContext('webgl') || canvas.getContext('experimental-webgl')));
     }());
 
-    //// Workaround for Intel GMA series (gl_FrontFacing causes compilation error)
-    //THREE.ShaderLib.lambert.fragmentShader = THREE.ShaderLib.lambert.fragmentShader.replace("gl_FrontFacing", "true");
-    //THREE.ShaderLib.lambert.vertexShader = THREE.ShaderLib.lambert.vertexShader.replace(/\}$/, "#ifdef DOUBLE_SIDED\n if (transformedNormal.z < 0.0) vLightFront = vLightBack;\n #endif\n }");
+    // Workaround for Intel GMA series (gl_FrontFacing causes compilation error)
+    THREE.ShaderLib.lambert.fragmentShader = THREE.ShaderLib.lambert.fragmentShader.replace("gl_FrontFacing", "true");
+    THREE.ShaderLib.lambert.vertexShader = THREE.ShaderLib.lambert.vertexShader.replace(/\}$/, "#ifdef DOUBLE_SIDED\n if (transformedNormal.z < 0.0) vLightFront = vLightBack;\n #endif\n }");
 
     TV3 = THREE.Vector3;
     TF3 = THREE.Face3;
